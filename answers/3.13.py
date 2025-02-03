a0d7b5a5324dab867f5ac1bdce68f7117268a59c
BriArl <brianna.youte@gmail.com>
2025-02-03 17:49:39 -0500

    fixed typo

--- answers/3.13.py
+++ answers/3.13.py
@@ -9,5 +9,5 @@ time = float(input())
 # Calculate calories burned using the given formula
 calories = (((age * 0.2757) + (weight * 0.03295) + (heart_rate * 1.0781) - 75.4991) * time) / 8.368
 
-# Output the result with two decial places
+# Output the result with two decimal places
 print(f'Calories: {calories:.2f} calories')
